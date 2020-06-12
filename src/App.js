d694e5d84fde00fb6e29712be35879e41b2b19f7
MohsinRaz3 <mohsinraz3@gmail.com>
2020-06-13 01:53:48 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Message } from './message.js'
 import './App.css';
 
-function App() {
+export default function Blm() {
+
+  let [count, setCount] = useState(1);
+  let [isOnn, setOnn] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isOnn ? 'dayLight' : ''}`}>
+
+      <div className= "alibml">
+
+      <button onClick={() => setOnn(!isOnn)}> 
+        Dark mode: {isOnn ? 'OFF' : 'ON'}
+      </button>  
+      </div>
+     
+      <h1>#BlackLivesMatter</h1>
+      
+
+      <Message counter={count} />
+      <img src="https://cdn.pixabay.com/photo/2012/05/07/02/12/down-47585_960_720.png" alt="Blacklivesmatter" title="Click To Pay Tribute"/>
+      <br />
+
+      <button onClick={() => setCount(count + 1)}>
+        1 Click = 1 Slap
+      </button>
+      <br/>
+      <br/>
+      <h3> "I can't breathe.."</h3>
+      <h1>#JusticeForGeorge</h1>
+      
+ 
+
+
+
     </div>
   );
 }
-
-export default App;
---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message (props){
+    return(
+        <h1>Click to Slap Racists: {props.counter}</h1>
+    )
+}
